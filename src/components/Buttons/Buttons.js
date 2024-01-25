e9346704f4e7a5ac2ed25288826042f79b3c197e
francesc pallares <fpallares@prensaiberica.es>
2024-01-25 16:50:09 +0100

    feat: all Disneypedia files

---
+++ src/components/Buttons/Buttons.js
@@ -0,0 +1,14 @@
+import './Buttons.css';
+
+const Buttons = () => {
+
+    return (
+        <div className='characters__buttons'>
+            <button className='characters__button' type="" value="">Anterior</button>
+            <span className='characters__page'>1</span>
+            <button className='characters__button' type="" value="">Siguiente</button>
+        </div>
+    );
+}
+
+export default Buttons;
---
+++ src/components/CharactersList/CharactersList.js
@@ -0,0 +1,84 @@
+import './CharactersList.css'
+
+const CharactersList = () => {
+
+    return (
+
+        <div className='characters'>
+            <div className='characters__card'>
+                <img className='characters__picture' src='https://static.wikia.nocookie.net/disney/images/7/77/9-eye.jpg' alt=''></img>
+                <p className='characters__title'>9-Eye</p>
+            </div>
+            <div className='characters__card'>
+                <img className='characters__picture' src='https://static.wikia.nocookie.net/disney/images/8/80/Profile_-_627.png' alt=''></img>
+                <p className='characters__title'>627</p>
+            </div>
+            <div className='characters__card'>
+                <img className='characters__picture' src='https://static.wikia.nocookie.net/disney/images/3/3f/90%27s_Adventure_Bear_profile.png' alt=''></img>
+                <p className='characters__title'>90's Adventure Bear</p>
+            </div>
+            <div className='characters__card'>
+                <img className='characters__picture' src='https://static.wikia.nocookie.net/disney/images/7/77/9-eye.jpg' alt=''></img>
+                <p className='characters__title'>9-Eye</p>
+            </div>
+            <div className='characters__card'>
+                <img className='characters__picture' src='https://static.wikia.nocookie.net/disney/images/8/80/Profile_-_627.png' alt=''></img>
+                <p className='characters__title'>627</p>
+            </div>
+            <div className='characters__card'>
+                <img className='characters__picture' src='https://static.wikia.nocookie.net/disney/images/3/3f/90%27s_Adventure_Bear_profile.png' alt=''></img>
+                <p className='characters__title'>90's Adventure Bear</p>
+            </div>
+            <div className='characters__card'>
+                <img className='characters__picture' src='https://static.wikia.nocookie.net/disney/images/7/77/9-eye.jpg' alt=''></img>
+                <p className='characters__title'>9-Eye</p>
+            </div>
+            <div className='characters__card'>
+                <img className='characters__picture' src='https://static.wikia.nocookie.net/disney/images/8/80/Profile_-_627.png' alt=''></img>
+                <p className='characters__title'>627</p>
+            </div>
+            <div className='characters__card'>
+                <img className='characters__picture' src='https://static.wikia.nocookie.net/disney/images/3/3f/90%27s_Adventure_Bear_profile.png' alt=''></img>
+                <p className='characters__title'>90's Adventure Bear</p>
+            </div>
+            <div className='characters__card'>
+                <img className='characters__picture' src='https://static.wikia.nocookie.net/disney/images/7/77/9-eye.jpg' alt=''></img>
+                <p className='characters__title'>9-Eye</p>
+            </div>
+            <div className='characters__card'>
+                <img className='characters__picture' src='https://static.wikia.nocookie.net/disney/images/8/80/Profile_-_627.png' alt=''></img>
+                <p className='characters__title'>627</p>
+            </div>
+            <div className='characters__card'>
+                <img className='characters__picture' src='https://static.wikia.nocookie.net/disney/images/3/3f/90%27s_Adventure_Bear_profile.png' alt=''></img>
+                <p className='characters__title'>90's Adventure Bear</p>
+            </div>
+            <div className='characters__card'>
+                <img className='characters__picture' src='https://static.wikia.nocookie.net/disney/images/3/3f/90%27s_Adventure_Bear_profile.png' alt=''></img>
+                <p className='characters__title'>90's Adventure Bear</p>
+            </div>
+            <div className='characters__card'>
+                <img className='characters__picture' src='https://static.wikia.nocookie.net/disney/images/8/80/Profile_-_627.png' alt=''></img>
+                <p className='characters__title'>627</p>
+            </div>
+            <div className='characters__card'>
+                <img className='characters__picture' src='https://static.wikia.nocookie.net/disney/images/3/3f/90%27s_Adventure_Bear_profile.png' alt=''></img>
+                <p className='characters__title'>90's Adventure Bear</p>
+            </div>
+            <div className='characters__card'>
+                <img className='characters__picture' src='https://static.wikia.nocookie.net/disney/images/3/3f/90%27s_Adventure_Bear_profile.png' alt=''></img>
+                <p className='characters__title'>90's Adventure Bear</p>
+            </div>
+            <div className='characters__card'>
+                <img className='characters__picture' src='https://static.wikia.nocookie.net/disney/images/3/3f/90%27s_Adventure_Bear_profile.png' alt=''></img>
+                <p className='characters__title'>90's Adventure Bear</p>
+            </div>
+            <div className='characters__card'>
+                <img className='characters__picture' src='https://static.wikia.nocookie.net/disney/images/3/3f/90%27s_Adventure_Bear_profile.png' alt=''></img>
+                <p className='characters__title'>90's Adventure Bear</p>
+            </div>
+        </div>
+    );
+}
+
+export default CharactersList;
